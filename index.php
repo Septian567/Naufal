483298e983459ed8797fd98b3fa0205d42cdd0c6
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-09-13 00:00:05 +0700

    .

--- index.php
+++ index.php
@@ -13,15 +13,29 @@
         <div class="greet">Selamat Ulang Tahun ke 22</div>
         <div>
             <article class="card center">
-                <img src="./asset/image/septina.jpg " class="featured-image " alt="foto ">
+                <div class="carousel-slider">
+                    <div class="slider">
+                        <img src="./asset/image/septina.jpg" alt="slider_1">
+                    </div>
+                    <div class="slider">
+                        <img src="./asset/image/septina2.jpeg" alt="slider_2">
+                    </div>
+                    <div class="slider">
+                        <img src="./asset/image/septina3.jpeg" alt="slider_3">
+                    </div>
+                    <a class="prev" onclick="move(-1)">&#10094;</a>
+                    <a class="next" onclick="move(1)">&#10095;</a>
+                </div>
+
                 <div class="name">Septina Shinta Monica</div>
+
 
             </article>
         </div>
     </header>
 
     <main>
-        <p>Selamat  Ulang Tahun</p>
+        <p>Selamat Ulang Tahun</p>
         <div class="audio">
             <audio controls>
                 <source src="./asset/audio/ucapan.mpeg" type="audio/mpeg">
@@ -37,9 +51,12 @@
     </main>
 
     <footer>
-    <p>ultah-22&#169;18-09-2022</p>
+        <p>ultah-22&#169;18-09-2022</p>
     </footer>
     <script src="asset/styles/script.js"></script>
+    <script>
+        window.alert("Selamat Ulang Tahun Septina Shinta Monica ...");
+    </script>
 </body>
 
 </html>
