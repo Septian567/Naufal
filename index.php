743ed9d33109319582a941c57774d7cb72f8bee9
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-09-12 07:32:33 +0700

    .

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./asset/image/balon.png">
+    <link rel="stylesheet" href="./asset/styles/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Selamat Ulang Tahun ...</title>
+</head>
+<body>
+    <h3>Selamat Ulang Tahun ke 22</h3>
+    <h2>Septina Shinta Monica</h2>
+    <footer>
+        <p>Website Opini Pribadi &#169; 2022</p>
+    </footer>
+    <script src="asset/styles/script.js"></script>
+</body>
+</html>
